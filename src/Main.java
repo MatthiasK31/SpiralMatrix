ba56db0ee18b83c95c438cb432f4197343f73a65
Matthias Kim <matthiastk805@gmail.com>
2022-03-13 16:01:45 -0400

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,36 @@
+/*
+Matthias Kim
+Mr. Hendricks
+3/13/22
+Extra: Allows user to enter a length of a spiral matrix they want to see
+ */
+
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.*;
 import static java.lang.System.*;
 
 public class Main
 {
     public static void main( String args[] ) throws IOException
     {
-        File file = new File("src/matrices.txt");
-        Scanner matrixReader = new Scanner(file);
-        int totalMatrices = 0; totalMatrices = matrixReader.nextInt();
+        //create scanner for file
+        Scanner matrixReader = new Scanner(new File("src/matrices.txt"));
 
-        SpiralMatrix [] matrices = new SpiralMatrix[totalMatrices];
-        for (int i = 1; i < totalMatrices; i++){
+        //arraylist of all the spiral matrices
+        ArrayList<SpiralMatrix> matrices = new ArrayList<>();
+
+        //loop through the file, passing in the size of the matrix then printing out the grid.
+        for (int i = 0; i < 6; i++){
             int matrixSize = matrixReader.nextInt();
-            matrices[i] = new SpiralMatrix(matrixSize);
-            out.println(matrices[i]);
+            matrices.add(new SpiralMatrix(matrixSize));
+            out.println(matrices.get(i));
         }
 
+        Scanner s = new Scanner(in);
+        out.print("Create a spiral matrix! How big would you like it to be?  ");
+        int size = s.nextInt();
+        out.println(new SpiralMatrix(size));
 
     }
 }
--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -4,11 +4,14 @@ import java.util.*;
 
 public class SpiralMatrix
 {
+    //size and mat definitions
     private int matrixSize;
     private int [][] mat;
+
     //define a matrix
     public SpiralMatrix(){}
 
+    //constructor to set matrix size and set size of matrix
     public SpiralMatrix(int size)
     {
         matrixSize = size;
@@ -22,29 +25,66 @@ public class SpiralMatrix
 
     public void createSpiral()
     {
+        //set starting and end positions for each of the array filling directions
         int counter = 1;
-        int down = matrixSize;
-        int right = matrixSize -1;
-        int up = matrixSize - 1;
-        int left = matrixSize - 2;
+        int downXStart = 0; int downYStart = 0; int downConstraint = matrixSize;
+        int rightXStart = 1; int rightYStart = matrixSize-1; int rightConstraint = matrixSize-1;
+        int upXStart = matrixSize-1; int upYStart = matrixSize-1; int upConstraint = 0;
+        int leftXStart = matrixSize-2; int leftYStart = 0; int leftConstraint = 1;
+
+
+        /* loop through until the number reaches the matrix size squared
+        For going down, the x and y start values increase while the loop length decreases
+        For going right, the x start value increases, the y start value decreases, and the loop length decreases
+        For going down, the x and y start values decrease while the loop length decreases
+        For going down, the x start value decreases, the y start value increases, and the loop length decreases
+         */
         do {
-            for (int d = 0; d < down; d++){
-                mat[d][0] = counter;
+            for (int d = downYStart; d < downConstraint; d++){
+                if (counter == Math.pow(matrixSize, 2)) break;
+                mat[d][downXStart] = counter;
                 counter++;
-            }
+            } downXStart++; downYStart++; downConstraint--;
+
+            for (int r = rightXStart; r < rightConstraint; r++) {
+                mat[rightYStart][r] = counter;
+                counter++;
+            } rightXStart++; rightYStart--; rightConstraint--;
+
+            for (int u = upYStart; u >= upConstraint; u--) {
+                mat[u][upXStart] = counter;
+                counter++;
+            } upXStart--; upYStart--; upConstraint++;
+
+            for (int l = leftXStart; l >= leftConstraint; l--){
+                mat[leftYStart][l] = counter;
+                counter++;
+            } leftXStart--; leftYStart++; leftConstraint++;
         } while (counter <= Math.pow(matrixSize, 2));
+    }
+
+    public void single(){
+        mat[0][0] = 1;
     }
 
     public String toString( )
     {
         String output="";
-        createSpiral();
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[i].length; j++){
-                out.print(mat[i][j]);
+
+        //if the matrix is greater than 1, create grid, otherwise print 1
+        if (matrixSize > 1)
+            createSpiral();
+        else if (matrixSize == 1)
+            single();
+
+        //loop through the mat and add on each of the numbers to the output return variable
+        for (int [] rows : mat){
+            for (int num : rows){
+                output += num + "\t\t";
             }
-            out.println();
+            output += "\n";
         }
+
         return output;
     }
 }
